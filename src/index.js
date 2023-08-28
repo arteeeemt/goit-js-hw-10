4a526f87728afffa41191911df8bf3db58b8f9f2
arteeeemt <kesha514@ukr.net>
2023-08-28 23:00:30 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_mkhQr4FHRFIVHQ0bJWYHd7zmtXfCmBCtCCScuFaYI5iKQOlZDc4EZSmh3QL2YcXJ"
+
 const selectInput = document.querySelector('.breed-select')
 const infoBlock = document.querySelector('.cat-info')
+const loader = document.querySelector('.loader')
+const errorBlock = document.querySelector('.error')
+
+
+loader.style.display = 'none'
+errorBlock.style.display = 'none'
 function fetchBreeds(){
-    return fetch('https://api.thecatapi.com/v1/breeds')
+    const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
+    return fetch(BASE_URL)
     .then((response) => {
+        if(!response.ok){
+            throw new Error
+        }
       return response.json();
     })
     .then((data) => {
@@ -13,25 +27,53 @@ function fetchBreeds(){
        selectInput.appendChild(option);
        
       })
-    });
+    })
+    .catch(error => {
+        errorBlock.style.display = 'inline'
+        
+    })
 }
 
 fetchBreeds()
 
 selectInput.addEventListener('change',function(){
     const selectedBreedId = selectInput.value
-    console.log(selectedBreedId)
+    
     fetchCatByBreed(selectedBreedId)
+    console.log(selectedBreedId)
 })
 function fetchCatByBreed(breedId){
-    
-    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    loader.style.display = 'inline'
+    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_mkhQr4FHRFIVHQ0bJWYHd7zmtXfCmBCtCCScuFaYI5iKQOlZDc4EZSmh3QL2YcXJ`)
     .then((response) => {
+       if(!response.ok){
+        throw new Error
+       }
       return response.json();
     })
     .then((data) => {
-      console.log(data)
-    });
+      createMarkup(data)
+    })
+    .catch(error => {
+        errorBlock.style.display = 'inline'
+        
+      })
+      .finally(() => {
+        
+        loader.style.display = 'none'
+      });
+
+    
+    
 }
 
-function createMarkup(){}
+function createMarkup(data){
+    const { url } = data[0];
+    const { name, description, temperament } = data[0].breeds[0]
+    infoBlock.innerHTML = `<img class=cat-img src="${url}" alt="Картинка">
+    <div><h1 class=header-title>${name}</h1>
+    <p class=desc>${description}</p>
+    <p class=temperament-desc>Temperament:${temperament}</p></div>`
+
+}
+
