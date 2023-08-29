f0a0fa57ffce5da85308cd3a315818d6c42df012
arteeeemt <kesha514@ukr.net>
2023-08-29 14:26:05 +0300

    add

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,26 +1,54 @@
+import {selectInput,infoBlock,errorBlock,loader} from "./index"
+import { createMarkup } from "./index"
+export function fetchBreeds(){
+  const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
+  return fetch(BASE_URL)
+  .then((response) => {
+      if(!response.ok){
+          throw new Error
+      }
+    return response.json();
+  })
+  .then((data) => {
+    data.map((cat)=>{
+      
+      const option = document.createElement('option')
+      option.value = cat.id;
+      option.textContent = cat.name;
+     selectInput.appendChild(option);
+     
+    })
+  })
+  .catch(error => {
+    
+      errorBlock.style.display = 'inline'
+      
+  })
+}
 
-import errorBlock from "/src/index.js";
+export function fetchCatByBreed(breedId){
+  loader.style.display = 'inline'
+  fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_mkhQr4FHRFIVHQ0bJWYHd7zmtXfCmBCtCCScuFaYI5iKQOlZDc4EZSmh3QL2YcXJ`)
+  .then((response) => {
+     if(!response.ok){
+      throw new Error
+      
+     }
+     errorBlock.style.display = 'none'
+    return response.json();
+  })
+  .then((data) => {
+    createMarkup(data)
+  
+  })
+  .catch(error => {
+      errorBlock.style.display = 'inline'
+      infoBlock.textContent = ''
+    })
+    .finally(() => {
+      loader.style.display = 'none'
+    });
 
-export default function fetchBreeds(){
-    const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
-    return fetch(BASE_URL)
-    .then((response) => {
-        if(!response.ok){
-            throw new Error
-        }
-      return response.json();
-    })
-    .then((data) => {
-      data.map((cat)=>{
-        const option = document.createElement('option')
-        option.value = cat.id;
-        option.textContent = cat.name;
-       selectInput.appendChild(option);
-       
-      })
-    })
-    .catch(error => {
-        errorBlock.style.display = 'inline'
-        
-    })
+  
+  
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
+
+ export const selectInput=document.querySelector('.breed-select')
+  export const infoBlock =document.querySelector('.cat-info')
+  export const errorBlock=document.querySelector('.error')
+  export const loader=document.querySelector('.loader')
+
+
 import axios from "axios";
-import fetchBreeds from './cat-api.js';
-
-
- const errorBlock = document.querySelector('.error')
- const selectInput = document.querySelector('.breed-select')
-const infoBlock = document.querySelector('.cat-info')
-const loader = document.querySelector('.loader')
-export default errorBlock 
-
-
 axios.defaults.headers.common["x-api-key"] = "live_mkhQr4FHRFIVHQ0bJWYHd7zmtXfCmBCtCCScuFaYI5iKQOlZDc4EZSmh3QL2YcXJ"
 
-
+import { fetchBreeds,fetchCatByBreed } from "./cat-api";
+ 
+// const selectInput = document.querySelector('.breed-select')
+// const infoBlock = document.querySelector('.cat-info')
+// const errorBlock = document.querySelector('.error')
+// const loader = document.querySelector('.loader')
 
 loader.style.display = 'none'
 errorBlock.style.display = 'none'
+
+
+
+
+window.addEventListener('DOMContentLoaded',fetchBreeds)
 
 // function fetchBreeds(){
 //     const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
@@ -40,7 +47,6 @@ errorBlock.style.display = 'none'
 //     })
 // }
 
-fetchBreeds()
 
 selectInput.addEventListener('change',function(){
     const selectedBreedId = selectInput.value
@@ -48,32 +54,35 @@ selectInput.addEventListener('change',function(){
     fetchCatByBreed(selectedBreedId)
     console.log(selectedBreedId)
 })
-function fetchCatByBreed(breedId){
-    loader.style.display = 'inline'
-    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_mkhQr4FHRFIVHQ0bJWYHd7zmtXfCmBCtCCScuFaYI5iKQOlZDc4EZSmh3QL2YcXJ`)
-    .then((response) => {
-       if(!response.ok){
-        throw new Error
-       }
-      return response.json();
-    })
-    .then((data) => {
-      createMarkup(data)
-    })
-    .catch(error => {
-        errorBlock.style.display = 'inline'
+
+// function fetchCatByBreed(breedId){
+//     loader.style.display = 'inline'
+//     fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_mkhQr4FHRFIVHQ0bJWYHd7zmtXfCmBCtCCScuFaYI5iKQOlZDc4EZSmh3QL2YcXJ`)
+//     .then((response) => {
+//        if(!response.ok){
+//         throw new Error
+//        }
+//        errorBlock.style.display = 'none'
+//       return response.json();
+//     })
+//     .then((data) => {
+//       createMarkup(data)
+      
+//     })
+//     .catch(error => {
+//         errorBlock.style.display = 'inline'
+//         infoBlock.textContent = ''
+//       })
+//       .finally(() => {
         
-      })
-      .finally(() => {
-        
-        loader.style.display = 'none'
-      });
+//         loader.style.display = 'none'
+//       });
 
     
     
-}
+// }
 
-function createMarkup(data){
+export function createMarkup(data){
     const { url } = data[0];
     const { name, description, temperament } = data[0].breeds[0]
     infoBlock.innerHTML = `<img class=cat-img src="${url}" alt="Картинка">
